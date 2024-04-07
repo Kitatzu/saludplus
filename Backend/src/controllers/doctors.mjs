96941f54f5b2432c20111b1c66090d21ef7e2d36
Kevin <kevinpernia29@gmail.com>
2024-04-07 18:04:23 -0500

    create/working on doctors' controllers

--- Backend/src/controllers/doctors.mjs
+++ Backend/src/controllers/doctors.mjs
@@ -1,30 +1,67 @@
 import express from "express";
+import bcrypt from "bcrypt";
+import Doctor from "../models/Doctor.mjs";
+
 const doctorsRouter = express.Router();
 
-doctorsRouter.get("/", (req, res) => {
+doctorsRouter.get("/", async(req, res) => {
     // TODO: Get all doctors from DB
     try {
-        res.send("Get all doctors");
+        const doctors = await Doctor.findAll({
+            attributes:{
+                exclude: [ "createdAt", "updatedAt"]
+            }
+        });
+        res.status(200).json({
+            success: true,
+            data: doctors
+        });
     } catch (error) {
-        
+        res.status(500).json({ error: error.message });
     }
 });
 
-doctorsRouter.post("/register", (req, res) => {
+doctorsRouter.post("/register", async(req, res) => {
     // TODO: Add new doctor to DB
     try {
-        
+        const { first_name, last_name, dni, registration, email, password } = req.body;
+        const doctor = await Doctor.create({
+            first_name,
+            last_name,
+            dni,
+            registration,
+            email,
+            password: await bcrypt.hash(password, 10)
+        });
+        res.status(201).json({
+            success: true,
+            data: doctor,
+            message: "Doctor created successfully"
+        });
     } catch (error) {
-        
+        res.status(500).json({
+            success: false,
+            error: error.message,
+            message: "Error registering doctor"
+        });
     }
 })
 
-doctorsRouter.get("/:id", (req, res) => {
+doctorsRouter.get("/:id", async(req, res) => {
     // TODO: Get doctor from DB
     try {
-        
+        const { id } = req.params;
+        const doctor = await Doctor.findByPk(id, {
+            attributes:{
+                exclude: ["password", "createdAt", "updatedAt"]
+            }
+        });
+        res.status(200).json({
+            success: true,
+            data: doctor
+        });
     } catch (error) {
-        
+        res.status(500).json({ error: error.message });
     }
 })
 
