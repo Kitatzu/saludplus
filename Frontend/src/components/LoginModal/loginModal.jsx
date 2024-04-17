4215608e09a7d88cee10f8c1f1689904bee6e335
Exequiel Rodriguez <exe922@gmail.com>
2024-04-17 13:36:49 -0300

    Fix modal style

--- Frontend/src/components/LoginModal/loginModal.jsx
+++ Frontend/src/components/LoginModal/loginModal.jsx
@@ -1,12 +1,13 @@
-'use client'
+"use client";
 
-import React from 'react';
-import './loginModalStyles.css';
+import React from "react";
+import "./loginModalStyles.css";
 
 const LoginModal = ({ isOpen, onClose }) => {
   // Función para cerrar el modal cuando se hace clic fuera de él
+
   const handleClickOutside = (event) => {
-    if (event.target.classList.contains('modal-overlay')) {
+    if (event.target.classList.contains("modal-overlay")) {
       onClose(); // Llama a la función onClose del componente padre para cerrar el modal
     }
   };
@@ -16,21 +17,23 @@ const LoginModal = ({ isOpen, onClose }) => {
     isOpen && (
       <div className="modal-overlay" onClick={handleClickOutside}>
         <div className="modal">
-          <h2>Iniciar Sesión</h2>
-          <form>
-            <label>
-              Usuario:
-              <input type="text" />
-            </label>
-            <label>
-              Contraseña:
-              <input type="password" />
-            </label>
-            <button type="submit">Iniciar Sesión</button>
-          </form>
           <button className="close-button" onClick={onClose}>
             X
           </button>
+          <div className="logo_container">
+            <img src="/Logo.png" alt="Logo Modal" />
+          </div>
+          <form>
+            <label>
+              <input type="email" placeholder="CORREO ELECTRÓNICO" />
+            </label>
+            <label>
+              <input type="password" placeholder="CONTRASEÑA" />
+            </label>
+            <button type="submit" className="login_btn">
+              Iniciar Sesión
+            </button>
+          </form>
         </div>
       </div>
     )
