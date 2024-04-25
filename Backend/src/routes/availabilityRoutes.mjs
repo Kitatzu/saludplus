5e230ed5ad2d9196a2d34ce629ababcc084f2374
Exequiel Rodriguez <exe922@gmail.com>
2024-04-24 23:09:41 -0300

    Doctor find available for speciality

--- Backend/src/routes/availabilityRoutes.mjs
+++ Backend/src/routes/availabilityRoutes.mjs
@@ -2,18 +2,12 @@ import { Router } from "express";
 import {
   getAllAvailability,
   createAvailability,
-  // updateAvailability,
-  // deleteAvailability,
-  getAvailabilityById,
 } from "../controllers/AvailabilitiesController.mjs";
 
 const router = Router();
 
 router
   .get("/available", getAllAvailability)
-  .post("/available", createAvailability)
-  // .patch("/available/:id", updateAvailability)
-  // .delete("/available/:id", deleteAvailability)
-  .get("/available/:id", getAvailabilityById);
+  .post("/available", createAvailability);
 
 export default router;
