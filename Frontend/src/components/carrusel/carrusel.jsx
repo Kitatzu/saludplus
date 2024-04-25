d0e1a59d86df83a54ed523c03992cc29fc58c0dc
Exequiel Rodriguez <exe922@gmail.com>
2024-04-24 23:25:38 -0300

    Backend deploy

--- Frontend/src/components/carrusel/carrusel.jsx
+++ Frontend/src/components/carrusel/carrusel.jsx
@@ -1,6 +1,6 @@
-/**"use client"
+"use client";
 import { useRef, useState, useEffect } from "react";
-import "./carrusel.css";
+import "./carrusel-style.css";
 export const Carrusel = () => {
   const sliderRef = useRef(null);
   const sliderIndividualRef = useRef([]);
@@ -11,23 +11,23 @@ export const Carrusel = () => {
   useEffect(() => {
     const slider = sliderRef.current;
     const sliderIndividual = sliderIndividualRef.current;
-    if (sliderIndividual.length > 0 && sliderIndividual[0]) {
-    width = sliderIndividual[0].clientWidth;
-
-    const resizeHandler = () => {
+    if (sliderIndividual.length > 0 && sliderIndividual[0]) {
       width = sliderIndividual[0].clientWidth;
-    };
 
-    window.addEventListener('resize', resizeHandler);
+      const resizeHandler = () => {
+        width = sliderIndividual[0].clientWidth;
+      };
 
-    const interval = setInterval(() => {
-      slides();
-    }, intervalo);
+      window.addEventListener("resize", resizeHandler);
 
-    return () => {
-      window.removeEventListener('resize', resizeHandler);
-      clearInterval(interval);
-        };
+      const interval = setInterval(() => {
+        slides();
+      }, intervalo);
+
+      return () => {
+        window.removeEventListener("resize", resizeHandler);
+        clearInterval(interval);
+      };
     }
   }, [sliderIndividualRef]);
 
@@ -36,59 +36,66 @@ export const Carrusel = () => {
     const sliderIndividual = sliderIndividualRef.current;
 
     slider.style.transform = `translate(${-width * contador}px)`;
-    slider.style.transition = 'transform .8s';
-    setContador(prevContador => (prevContador + 1) % sliderIndividual.length);
-  };
-    return(
-        <div className="slider-contenedor" ref={sliderRef}>
-            {sliderIndividualRef.current.map((section, index) => (
-        <div key={index} className="content-slider" ref={el => sliderIndividualRef.current[index] = el}>
-            <section>
+    slider.style.transition = "transform .8s";
+    setContador((prevContador) => (prevContador + 1) % sliderIndividual.length);
+  };
+  return (
+    <div className="slider-contenedor" ref={sliderRef}>
+      {sliderIndividualRef.current.map((section, index) => (
+        <div
+          key={index}
+          className="content-slider"
+          ref={(el) => (sliderIndividualRef.current[index] = el)}
+        >
+          <section>
             <div>
-                <h1>
-                    Autogestión
-                </h1>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet illum ducimus odit sapiente similique? Debitis corrupti harum rerum? Eum amet rerum quisquam quos quaerat harum, veniam quas commodi possimus quibusdam?
-                </p>
+              <h1>Autogestión</h1>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet
+                illum ducimus odit sapiente similique? Debitis corrupti harum
+                rerum? Eum amet rerum quisquam quos quaerat harum, veniam quas
+                commodi possimus quibusdam?
+              </p>
             </div>
             <div>
-                <img src="/images/Vector.svg" alt="vector"/>
+              <img src="/images/Vector.svg" alt="vector" />
             </div>
-            </section>
+          </section>
 
-            <section>
+          <section>
             <div>
-                <h1>
-                    Chat / Videollamada
-                </h1>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet illum ducimus odit sapiente similique? Debitis corrupti harum rerum? Eum amet rerum quisquam quos quaerat harum, veniam quas commodi possimus quibusdam?
-                </p>
+              <h1>Chat / Videollamada</h1>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet
+                illum ducimus odit sapiente similique? Debitis corrupti harum
+                rerum? Eum amet rerum quisquam quos quaerat harum, veniam quas
+                commodi possimus quibusdam?
+              </p>
             </div>
             <div>
-                <img src="/images/Vector.svg" alt="vector"/>
+              <img src="/images/Vector.svg" alt="vector" />
             </div>
-            </section>
+          </section>
 
-            <section>
+          <section>
             <div>
-                <h1>
-                    Historial
-                </h1>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet illum ducimus odit sapiente similique? Debitis corrupti harum rerum? Eum amet rerum quisquam quos quaerat harum, veniam quas commodi possimus quibusdam?
-                </p>
+              <h1>Historial</h1>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet
+                illum ducimus odit sapiente similique? Debitis corrupti harum
+                rerum? Eum amet rerum quisquam quos quaerat harum, veniam quas
+                commodi possimus quibusdam?
+              </p>
             </div>
             <div>
-                <img src="/images/Vector.svg" alt="vector"/>
+              <img src="/images/Vector.svg" alt="vector" />
             </div>
-            </section>
-            <div className="button">
-                <button>-</button> <button>+</button>
-            </div>
+          </section>
+          <div className="button">
+            <button>-</button> <button>+</button>
+          </div>
         </div>
-      ))}
-    </div>
-    )
-
+      ))}
+    </div>
+  );
+};
