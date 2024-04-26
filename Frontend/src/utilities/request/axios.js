a15e2f88065fad5090a4e04c01325cf341179374
Exequiel Rodriguez <exe922@gmail.com>
2024-04-25 23:28:08 -0300

    Add Fix backend

--- Frontend/src/utilities/request/axios.js
+++ Frontend/src/utilities/request/axios.js
@@ -3,6 +3,20 @@ import { jwtDecode } from "jwt-decode";
 
 const url = process.env.NEXT_PUBLIC_URL;
 
+export const addAppoiment = async (data) => {
+  if (!data || data.length === 0) {
+    throw new Error("No data submited");
+  }
+
+  try {
+    const response = await axios.post(`${url}appoiment`, data);
+
+    return response;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 export const fetchAppoiments = async (token) => {
   if (!token) {
     throw new Error("Token is required");
